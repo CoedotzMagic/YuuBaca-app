61d24427860cd1211f908443d0fee680f9ac884c
poncoe <poncoe4mac@gmail.com>
2022-05-08 04:49:35 +0700

    create model buku

---
+++ app/src/main/java/id/standherealone/yuubaca/model/Buku.kt
@@ -0,0 +1,10 @@
+package id.standherealone.yuubaca.model
+
+data class Buku (
+    var isbn: String,
+    var gambar: String,
+    var judul: String,
+    var kategori: String,
+    var tingkatan: String,
+    var file: String,
+)
