d4d1a6e1d6ae6bf41995cb49d0579ee8308d935a
poncoe <poncoe4mac@gmail.com>
2022-05-08 04:55:18 +0700

    added PicassoClient.kt for handling image from data api

---
+++ app/src/main/java/id/standherealone/yuubaca/core/PicassoClient.kt
@@ -0,0 +1,16 @@
+package id.standherealone.yuubaca.core
+
+import android.content.Context
+import android.widget.ImageView
+import com.squareup.picasso.Picasso
+import id.standherealone.yuubaca.R
+
+object PicassoClient {
+    fun downloadImage(c: Context?, url: String?, img: ImageView?) {
+        if (url != null && url.isNotEmpty()) {
+            Picasso.get().load(url).placeholder(R.mipmap.ic_launcher).into(img)
+        } else {
+            Picasso.get().load(R.mipmap.ic_launcher).into(img)
+        }
+    }
+}
