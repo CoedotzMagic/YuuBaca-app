b73dd52d3008d20f0fe5072be01f51184e78edef
poncoe <poncoe4mac@gmail.com>
2022-05-12 03:09:33 +0700

    implement retrofit rest api & recive data from yuubaca api

--- app/src/main/java/id/standherealone/yuubaca/ui/home/HomeFragment.kt
+++ app/src/main/java/id/standherealone/yuubaca/ui/home/HomeFragment.kt
@@ -6,12 +6,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import id.standherealone.yuubaca.R
+import id.standherealone.yuubaca.api.ApiBuku
+import id.standherealone.yuubaca.core.adapter.BukuAdapter
 import id.standherealone.yuubaca.databinding.FragmentHomeBinding
+import id.standherealone.yuubaca.model.Buku
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class HomeFragment : Fragment() {
 
     private lateinit var binding: FragmentHomeBinding
     private lateinit var viewModel: HomeViewModel
+    lateinit var recyclerView: RecyclerView
+    lateinit var recyclerAdapter: BukuAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,6 +37,27 @@ class HomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
         // TODO: Use the ViewModel
+
+        recyclerView = binding.recyclerview
+        recyclerAdapter = BukuAdapter(requireContext())
+        binding.recyclerview.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter = recyclerAdapter
+
+
+        val apiInterface = ApiBuku.create().getBuku()
+
+        //apiInterface.enqueue( Callback<List<Buku>>())
+        apiInterface.enqueue( object : Callback<List<Buku>> {
+            override fun onResponse(call: Call<List<Buku>>?, response: Response<List<Buku>>?) {
+
+                if(response?.body() != null)
+                    recyclerAdapter.setBukuListItems(response.body()!!)
+            }
+
+            override fun onFailure(call: Call<List<Buku>>?, t: Throwable?) {
+
+            }
+        })
     }
 
 }
