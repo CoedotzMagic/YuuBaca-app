0d9bb7e3043f67042c7850985011361e3e06fb8e
poncoe <poncoe4mac@gmail.com>
2022-05-19 03:26:21 +0700

    implement search for filter data

--- app/src/main/java/id/standherealone/yuubaca/ui/category/CategorySMPActivity.kt
+++ app/src/main/java/id/standherealone/yuubaca/ui/category/CategorySMPActivity.kt
@@ -1,9 +1,14 @@
 package id.standherealone.yuubaca.ui.category
 
+import android.app.SearchManager
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -24,6 +29,8 @@ class CategorySMPActivity : AppCompatActivity() {
     lateinit var recyclerView: RecyclerView
     lateinit var recyclerAdapter: BukuAdapter
     private var shimmer: ShimmerFrameLayout? = null
+    lateinit var searchView: SearchView
+    lateinit var bukuList: List<Buku>
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,14 +45,19 @@ class CategorySMPActivity : AppCompatActivity() {
         recyclerView.layoutManager = GridLayoutManager(this, 2)
         recyclerView.adapter = recyclerAdapter
 
+        bukuList = ArrayList<Buku>()
         val apiInterface = ApiSMP.create().getBuku()
 
         //apiInterface.enqueue( Callback<List<Buku>>())
         apiInterface.enqueue(object : Callback<List<Buku>> {
             override fun onResponse(call: Call<List<Buku>>?, response: Response<List<Buku>>?) {
 
-                if (response?.body() != null)
-                    recyclerAdapter.setBukuListItems(response.body()!!)
+//                if (response?.body() != null)
+//                    recyclerAdapter.setBukuListItems(response.body()!!)
+
+                bukuList = response!!.body()!!
+                Log.d("TAG", "Response = $bukuList")
+                recyclerAdapter.setBukuList(applicationContext, bukuList)
 
                 //shimmer
                 shimmer!!.stopShimmer()
@@ -65,6 +77,37 @@ class CategorySMPActivity : AppCompatActivity() {
         })
     }
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        val searchManager = getSystemService(SEARCH_SERVICE) as SearchManager
+        searchView = menu.findItem(R.id.action_search)
+            .actionView as SearchView
+        searchView.setSearchableInfo(
+            searchManager
+                .getSearchableInfo(componentName)
+        )
+        searchView.maxWidth = Int.MAX_VALUE
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                recyclerAdapter.getFilter()?.filter(query)
+                return false
+            }
+
+            override fun onQueryTextChange(query: String): Boolean {
+                recyclerAdapter.getFilter()?.filter(query)
+                return false
+            }
+        })
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        return if (id == R.id.action_search) {
+            true
+        } else super.onOptionsItemSelected(item)
+    }
+
     override fun onResume() {
         super.onResume()
         shimmer!!.startShimmer()
@@ -76,6 +119,11 @@ class CategorySMPActivity : AppCompatActivity() {
     }
 
     override fun onBackPressed() {
+        if (!searchView.isIconified) {
+            searchView.isIconified = true
+            return
+        }
+        super.onBackPressed()
         this.finish()
     }
 
